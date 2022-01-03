957869b4315b2954458a2df15a572863a3f91c87
zebwa <azusmani86@gmail.com>
2022-01-03 17:01:23 +0530

    first automation 3rd jan

---
+++ src/main/java/com/qa/crm/pages/GitDemo.java
@@ -0,0 +1,13 @@
+package com.qa.crm.pages;
+
+public class GitDemo {
+	
+	public void sum() {
+		System.out.println("this is a sum method...");
+	}
+	
+	public void sub() {
+		System.out.println("this is a substraction method...");
+	}
+
+}
