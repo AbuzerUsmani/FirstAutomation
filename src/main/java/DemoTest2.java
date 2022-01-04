61b762e9c28ebf6d3041744d9db1569e0e36817a
azusmani86@gmail.com <azusmani86@gmail.com>
2022-01-04 17:03:12 +0530

    new class has been added in same package by American guy

---
+++ src/main/java/DemoTest2.java
@@ -0,0 +1,11 @@
+
+public class DemoTest2 {
+	
+	public void registration() {
+		System.out.println("this is a registration page");
+	}
+
+	public void checkout() {
+		System.out.println("this is a checkout page");
+	}
+}
