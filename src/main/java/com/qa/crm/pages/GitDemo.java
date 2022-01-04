e3bcb230de08f4efe562ba2b84967f5a60103982
zebwa <azusmani86@gmail.com>
2022-01-04 13:22:09 +0530

    deleted some dots in gitdemo class

--- src/main/java/com/qa/crm/pages/GitDemo.java
+++ src/main/java/com/qa/crm/pages/GitDemo.java
@@ -3,11 +3,10 @@ package com.qa.crm.pages;
 public class GitDemo {
 	
 	public void sum() {
-		System.out.println("this is a sum method...");
+		System.out.println("this is a sum method.");
 	}
 	
 	public void sub() {
-		System.out.println("this is a substraction method...");
+		System.out.println("this is a substraction method.");
 	}
-
 }
