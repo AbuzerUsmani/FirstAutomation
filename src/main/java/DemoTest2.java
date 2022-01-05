0c7671580f5c84887f0bf64a3096d4cae78bea3e
azusmani86@gmail.com <azusmani86@gmail.com>
2022-01-05 14:33:22 +0530

    added payment method by american guy

--- src/main/java/DemoTest2.java
+++ src/main/java/DemoTest2.java
@@ -4,8 +4,12 @@ public class DemoTest2 {
 	public void registration() {
 		System.out.println("this is a registration page");
 	}
-
+	
 	public void checkout() {
 		System.out.println("this is a checkout page");
+	}
+	
+	public void payment() {
+		System.out.println("this is a payment method");
 	}
 }
