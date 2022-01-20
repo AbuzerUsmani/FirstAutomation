e1d14edf9df3ec2d8979f206095775e691fe9227
azusmani86@gmail.com <azusmani86@gmail.com>
2022-01-20 16:36:42 +0530

    adding swapping program

---
+++ src/test/java/InterviewDemos/SwappingNum.java
@@ -0,0 +1,22 @@
+package InterviewDemos;
+
+public class SwappingNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		int a = 10;
+		int b = 20;
+		int temp;
+		
+		temp = a;
+		a = b;
+		b= temp;
+		
+		System.out.println("a :"+ a);
+		System.out.println("b :"+ b);
+
+	}
+
+}
