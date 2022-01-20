b2a357e60e6e4b39f69717c72314085055a1551f
azusmani86@gmail.com <azusmani86@gmail.com>
2022-01-21 03:30:18 +0530

    hello user

--- src/test/java/InterviewDemo/Test1.java
+++ src/test/java/InterviewDemo/Test1.java
@@ -1,10 +1,19 @@
 package InterviewDemo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi is this?");
+		System.setProperty("webdriver.chrome.driver", "E:\\study\\FirstAutomation"
+				+ "\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.rahulshettyacademy.com/");
+		
+		driver.manage().window().maximize();
 
 	}
 
